9322741679675fe674dc5dbb402343c4f8fee2ed
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-08-21 23:04:21 -0300

    Working With Tdd and xUnit

---
+++ WorkingWithTddXUnit/CursoOnline/CursoOnline.Testes/Dominio/Curso/CursoTeste.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+#region Caso de Uso
+
+    /* Eu, enquanto administrador, quero criar e editar cursos para que sejam abertas matrículas para o mesmo.
+     * 
+     * DoD (Definition of Done) = Critério de aceite:
+     *  => Criar um curso com nome, carga horária, público alvo e valor de curso;
+     *  => As opções para público alvo são: Estudante, Universitário, Empregado e Empreendedor
+     *  => Todos os campos do curso são obrigatórios.
+     */
+
+#endregion
+
+namespace CursoOnline.Teste.Dominio.Curso
+{
+    public class CursoTeste
+    {
+        [Fact]
+        public void DeveCriarCurso()
+        {
+
+        }
+    }
+}
