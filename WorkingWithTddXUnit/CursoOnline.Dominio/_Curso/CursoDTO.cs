b5421c0c30c377dac2ef424993734dc25b2ab74e
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-09-21 17:19:18 -0300

    Testes mais complexos e ajustes na arquitetura

---
+++ WorkingWithTddXUnit/CursoOnline.Dominio/_Curso/CursoDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace CursoOnline.Dominio._Curso
+{
+    public class CursoDTO
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double CargaHoraria { get; set; }
+        public string PublicoAlvo { get; set; }
+        public double Valor { get; set; }
+
+        public CursoDTO()
+        {
+        }
+    }
+}
---
+++ WorkingWithTddXUnit/CursoOnline.Dominio/_Service/ArmazenadorDeCurso.cs
@@ -0,0 +1,30 @@
+﻿using CursoOnline.Dominio._Curso;
+using System;
+
+namespace CursoOnline.Dominio._Curso
+{
+    public class ArmazenadorDeCurso
+    {
+        private readonly ICursoRepositorio _cursoRepositorio;
+
+        public ArmazenadorDeCurso(ICursoRepositorio cursoRespositorio)
+        {
+            _cursoRepositorio = cursoRespositorio;
+        }
+
+        public void Armazenar(CursoDTO cursoDTO)
+        {
+            var cursoJaSalvo = _cursoRepositorio.ObterPeloNome(cursoDTO.Nome);
+
+            if (cursoJaSalvo != null)
+                throw new ArgumentException("Nome do curso já consta no banco de dados!");
+
+            if (!Enum.TryParse<PublicoAlvo>(cursoDTO.PublicoAlvo, out var publicoAlvo))
+                throw new ArgumentException("Público alvo inválido!");
+
+            var curso 
+                = new Curso(cursoDTO.Nome, cursoDTO.CargaHoraria, cursoDTO.Descricao, publicoAlvo, cursoDTO.Valor);
+            _cursoRepositorio.Adicionar(curso);
+        }
+    }
+}
