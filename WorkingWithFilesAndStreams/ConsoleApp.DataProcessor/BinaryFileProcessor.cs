735e30f9a7263fbbcb3d4c73167246ed6d169b98
Emanuel Neto <ti.emanuelneto@yahoo.com.br>
2020-11-02 17:39:56 -0300

    Pluralsight Course - Final

--- WorkingWithFilesAndStreams/ConsoleApp.DataProcessor/BinaryFileProcessor.cs
+++ WorkingWithFilesAndStreams/ConsoleApp.DataProcessor/BinaryFileProcessor.cs
@@ -1,29 +1,68 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.IO.Abstractions;
 
 namespace ConsoleApp.DataProcessor
 {
     public class BinaryFileProcessor
     {
+        private readonly IFileSystem _fileSystem;
+
         public string InputFilePath { get; set; }
         public string OutputFilePath { get; set; }
 
         public BinaryFileProcessor(string inputFilePath, string outputFilePath)
+            : this(inputFilePath, outputFilePath, new FileSystem()) { }
+
+        public BinaryFileProcessor(string inputFilePath, string outputFilePath, IFileSystem fileSystem)
         {
             this.InputFilePath = inputFilePath;
             this.OutputFilePath = outputFilePath;
+            this._fileSystem = fileSystem;
         }
 
         public void Process()
         {
-            byte[] data = File.ReadAllBytes(InputFilePath);
-            byte largest = data.Max();
-            byte[] newData = new byte[data.Length + 1];
-            Array.Copy(data, newData, data.Length);
-            newData[newData.Length - 1] = largest;
+            #region Way 1
 
-            File.WriteAllBytes(OutputFilePath, newData);
+            //byte[] data = File.ReadAllBytes(InputFilePath);
+            //byte largest = data.Max();
+            //byte[] newData = new byte[data.Length + 1];
+            //Array.Copy(data, newData, data.Length);
+            //newData[newData.Length - 1] = largest;
+
+            //File.WriteAllBytes(OutputFilePath, newData);
+
+            #endregion
+
+            #region Way 2 - Test Preparative
+
+            using (Stream inputFileStream = _fileSystem.File.Open(InputFilePath, FileMode.Open, FileAccess.Read))
+            using (BinaryReader binarytStreamReader = new BinaryReader(inputFileStream))
+            using (Stream outputFileStream = _fileSystem.File.Create(OutputFilePath))
+            using (BinaryWriter binaryStreamWriter = new BinaryWriter(outputFileStream))
+            {
+                int largest = 0;
+
+                while (binarytStreamReader.BaseStream.Position < binarytStreamReader.BaseStream.Length)
+                {
+                    int currentByte = binarytStreamReader.ReadByte();
+                    binaryStreamWriter.Write(currentByte);
+
+                    if (currentByte > largest)
+                    {
+                        largest = currentByte;
+                    }
+                }
+
+                binaryStreamWriter.Write(largest);
+            }
+
+
+
+            #endregion
+
         }
     }
 }
---
+++ WorkingWithFilesAndStreams/DataProcessor.Tests/TextFileProcessorShould.cs
@@ -0,0 +1,39 @@
+﻿using Xunit;
+using System.IO.Abstractions.TestingHelpers;
+using ConsoleApp.DataProcessor;
+
+namespace DataProcessor.Tests
+{
+    public class TextFileProcessorShould
+    {
+        [Fact]
+        public void MakeTheSecondLineUpperCase()
+        {
+            //Create a mock input file
+            var mockInputFile = new MockFileData("Line 1\nLine 2\nLine 3");
+
+            //Setup mock file system starting state
+            var mockFileSystem = new MockFileSystem();
+            mockFileSystem.AddFile(@"c:\rootTest\in\myFile.txt", mockInputFile);
+            mockFileSystem.AddDirectory(@"c:\rootTest\out");
+
+            //Create TextFileProcessor with mock file system
+            var sut = new TextFileProcessor(@"c:\rootTest\in\myFile.txt"
+                                            , @"c:\rootTest\out\myFile.txt" 
+                                            , mockFileSystem);
+
+            //Process Test File
+            sut.Process();
+
+            //Check mock file system for output file
+            Assert.True(mockFileSystem.FileExists(@"c:\rootTest\out\myFile.txt"));
+
+            //Check content of output file in mock file system
+            MockFileData processedFile = mockFileSystem.GetFile(@"c:\rootTest\out\myFile.txt");
+            string[] lines = processedFile.TextContents.SplitLines();
+            Assert.Equal("Line 1", lines[0]);
+            Assert.Equal("LINE 2", lines[1]);
+            Assert.Equal("Line 3", lines[2]);
+        }
+    }
+}
